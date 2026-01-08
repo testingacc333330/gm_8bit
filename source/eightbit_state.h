2df88eb1314d661f690d0d8d32f77ada67b9c0b1
Augaton <Mulleraurelien@hotmail.fr>
2026-01-08 13:25:26 +0100

    [Fix] Passage de la structure de PlayerState en externe

--- source/eightbit_state.h
+++ source/eightbit_state.h
@@ -2,6 +2,11 @@
 #include <string>
 #include <unordered_map>
 
+struct PlayerState {
+	IVoiceCodec* codec = nullptr;
+	int effect = 0;
+};
+
 struct EightbitState {
 	int crushFactor = 350;
 	float gainFactor = 1.2;
@@ -9,9 +14,5 @@ struct EightbitState {
 	int desampleRate = 2;
 	uint16_t port = 4000;
 	std::string ip = "127.0.0.1";
-	struct PlayerState {
-		IVoiceCodec* codec = nullptr;
-		int effect = 0;
-	};
 	PlayerState afflictedPlayers[129]; // 0 à 128
 };
